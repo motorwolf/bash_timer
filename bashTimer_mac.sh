4218c01d9f3f5a8693156dc24a9781d62ec66a9e
Kristen Payne <kristen@polysign.io>
2019-05-21 17:31:54 -0700

    added some mac specific goodies

---
+++ bashTimer_mac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+startTime=$(date +"%T %F")
+read -p "What will you be doing? " task
+read -p "How many minutes? " originalMin
+clear
+echo "==========================="
+echo "CURRENT TASK: " $task
+min=$((originalMin - 1))
+secs=$((60))
+while [ $min -gt 0 ]; do
+while [ $secs -gt 0 ]; do
+   echo -ne "$min minutes $secs seconds \033[0K\r"
+   sleep 1
+   : $((secs--))
+done
+min=$((min - 1))
+secs=$((60))
+done
+#endTime=$(date +"%T %F")
+echo "$task : $originalMin minutes" >> ~/dailylog.txt
+echo "$startTime" >> ~/dailylog.txt
+#echo "$endTime" >> ~/dailylog.txt
+cat ~/dailylog.txt
+osascript -e 'display notification "Timer is done!"'
+#exec notify-send "Time's up!" 
+#exec mpv ~/audio/other/sound-effects/tibetian-singing-bowl.wav
+
